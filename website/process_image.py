68dd0b414b082e1e65d61e216f871da2e0b2acac
Armon <armon@nudgex.net>
2022-08-02 22:11:03 -0700

    made angles act on each frame

--- website/process_image.py
+++ website/process_image.py
@@ -3,14 +3,14 @@ import halftone as ht
 import pdb
 import os
 
-def process_image(path_to_image, spacing=14, angle=30):
+def process_image(path_to_image, spacing=14, angles = [0,45,70]):
     """
     this is the function actually called from flask
     processes image, then saves image to website/images/processed/{case_code}.png
     """
     unprocessed_image = Image.open(path_to_image)
 
-    processed_image = process_image_2(unprocessed_image, spacing, angle, dotfun=ht.euclid_dot) #this part looks good
+    processed_image = process_image_2(unprocessed_image, spacing, angles, dotfun=ht.euclid_dot) #this part looks good
 
     path_to_processed_image = path_to_image.replace('/unprocessed/', '/processed/')
     processed_image.save(path_to_processed_image)
@@ -38,16 +38,28 @@ def process_image(path_to_image, spacing=14, angle=30):
 
     return None
 
-def process_image_2(unprocessed_image, spacing=14, angle=30, dotfun=ht.euclid_dot):
+def process_image_2(unprocessed_image, spacing=14, angles = [0, 45, 70], dotfun=ht.euclid_dot):
     """
     takes in color image and processes that bad boi
     """
     mode = unprocessed_image.mode
     channels = unprocessed_image.split()
+
+    if unprocessed_image.mode == 'RGBA':
+        original_alpha = channels[-1]
+        channels = channels[:-1]
+
     halftone_channels = []
+
+    angles = [0,45,70]
+    angle_iter = iter(angles)
     for channel in channels:
+        angle = next(angle_iter)
         halftone_channel = ht.halftone(channel, dotfun(spacing=spacing, angle=angle))
         halftone_channels.append(halftone_channel)
+
+    if unprocessed_image.mode == 'RGBA':
+        halftone_channels.append(original_alpha)
 
     return Image.merge(mode, halftone_channels)
 
