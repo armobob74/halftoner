fc9e47f1f68484e6d93e81424824f9d9034b810d
Armon <armon@nudgex.net>
2022-08-03 00:39:46 -0700

    got black and white SVG halftone working. No option to adjust screen angle yet.

--- website/svg_experiments/process_image.py
+++ website/svg_experiments/process_image.py
@@ -1,26 +1,77 @@
 from PIL import Image
+import pdb
 import numpy as np
 
-def load_image(path,mode='RGBA'):
+def load_image(path, mode='RGBA'):
     image = Image.open(path)
     return image.convert(mode)
 
-def calculate_r_matrix(image, ):
+def pad_image(image, grid_block_size=9):
+    """
+    makes sure that the image will evenly divide into grids of size grid_block_size
+    """
+    desired_height = int(np.ceil(image.height / grid_block_size) * grid_block_size)
+    desired_width = int(np.ceil(image.width / grid_block_size) * grid_block_size)
+
+    width_delta = desired_width - image.width
+    height_delta = desired_height - image.height
+
+    top = int(np.floor(height_delta / 2))
+    left = int(np.floor(width_delta / 2))
+
+    bands = image.getbands()
+
+    padding_color = [255] * len(bands)
+    if image.mode == 'RGBA':
+        padding_color[-1] = 0
+    
+    padding_color = tuple(padding_color)
+    padding = Image.new(image.mode, (desired_width, desired_height), color=padding_color)
+    padding.paste(image, (left, top))
+
+    return padding
+
+    
+
+def calculate_dot_r(selection: np.array, grid_block_size):
+    white_area = np.sum(selection)
+    black_area = 255 * selection.size - white_area
+    r = np.sqrt(black_area / np.pi)
+    return r
+
+
+def calculate_svg_dots(image, dot_size=8, spacing=0):
     """
     takes in a greyscale image and returns an r_matrix
     """
-    r_matrix = None
-    return r_matrix
+    #upper left hand corner is (i,j)
+    svg_dots = []
+    img_arr = np.array(image)
+    for i in range(0,image.height,dot_size):
+        for j in range(0, image.width,dot_size):
+            selection = img_arr[i:(i+dot_size), j:(j+dot_size)]
 
-def make_svg_str(r_matrix=[[1,2,3]], spacing=10, dotsize=1):
-    svg_str = f'<svg viewBox="0 0 {len(r_matrix[0])*spacing} {len(r_matrix)*spacing}" xmlns="http://www.w3.org/2000/svg">'
-    for i in range(len(r_matrix)):
-        for j in range(len(r_matrix[0])):
-            svg_str += f'\n<circle cx="{j * spacing}" cy="{i * spacing}" r="{r_matrix[i][j] * dotsize}"/>'
-    svg_str += '\n</svg>'
-    return svg_str
+            r = calculate_dot_r(selection, dot_size)
+            grid_len = dot_size + spacing #length of a standard grid block
+            svg_dot = f'<circle cx="{j * grid_len}" cy="{i * grid_len}" r="{r:.3}"/>'
+            svg_dots.append(svg_dot)
+
+    return '\n'.join(svg_dots)
+
+def halftonify(image, dot_size=8, spacing=0):
+    grid_block_size = dot_size + spacing
+    image = pad_image(image, grid_block_size)
+    channels = image.split()
+
+    if len(channels) > 3:
+        channels = channels[:3]
+    for channel in channels:
+        svg_dots = calculate_svg_dots(image, dot_size, spacing)
+
+    svg_head = f'<svg viewBox="0 0 {image.width * grid_block_size} {image.height * grid_block_size}" xmlns="http://www.w3.org/2000/svg">'
+    return '\n'.join([svg_head, svg_dots, '</svg>'])
 
 
 if __name__ == "__main__":  
-    print(make_svg_str())
-    image = load_image(path='onion.png', mode='RGBA')
+    image = load_image('fakeface1.jpg', 'L')
+    print(halftonify(image, dot_size=12, spacing=6))
