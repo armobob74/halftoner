40f7185909c64d2d94b04921b2216ffc5f0ec472
Armon <armon@nudgex.net>
2022-08-13 00:53:21 -0700

    Made program use default value if an empty field was submitted. Made black generation default to 0.

--- website/views.py
+++ website/views.py
@@ -9,12 +9,28 @@ views = Blueprint('views', __name__)
 
 @views.route('/',methods=['GET','POST'])
 def index():
+    defaults = {
+                    'spacing': 8,
+                    'black_generation': 0.0,
+                    'angle_cyan':165,
+                    'angle_magenta':45,
+                    'angle_yellow':90,
+                    'angle_black':105,
+    }
     if request.method == 'POST':
+
+        #if form value is empty, use default value instead
+        processed_form = dict()
+        for key in request.form.keys():
+            if request.form[key] == '':
+                processed_form[key] = defaults[key]
+            else:
+                processed_form[key] = request.form[key]
 
         #probably could do this with a redirect or something, but case_code=="None" means 
         #the user is submitting image for first time
         #otherwise, it is a redo.
-        case_code = request.form['case_code']
+        case_code = processed_form['case_code']
         if case_code == "None":
             case_code = randstr(16).lower()#this string MUST contain no upper case letters
             pic = request.files['evidence']
@@ -23,13 +39,13 @@ def index():
         else:
             filepath = f'website/static/images/unprocessed/{case_code}.png'
 
-        spacing = float(request.form['spacing'])
-        black_generation = float(request.form['black_generation'])
+        spacing = float(processed_form['spacing'])
+        black_generation = float(processed_form['black_generation'])
         angles = [
-                    float(request.form['angle_cyan']),
-                    float(request.form['angle_magenta']),
-                    float(request.form['angle_yellow']),
-                    float(request.form['angle_black'])
+                    float(processed_form['angle_cyan']),
+                    float(processed_form['angle_magenta']),
+                    float(processed_form['angle_yellow']),
+                    float(processed_form['angle_black'])
                 ]
         process_image(filepath, spacing=spacing, angles=angles, black_generation=black_generation)
         image = Image.open(filepath)
@@ -37,22 +53,14 @@ def index():
         imgwidth = 20 #vw
         imgheight = imgwidth * image.height / image.width
         defaults = {
-                    'spacing': float(request.form['spacing']),
-                    'black_generation': float(request.form['black_generation']),
-                    'angle_cyan':float(request.form['angle_cyan']),
-                    'angle_magenta':float(request.form['angle_magenta']),
-                    'angle_yellow':float(request.form['angle_yellow']),
-                    'angle_black':float(request.form['angle_black']),
+                    'spacing': float(processed_form['spacing']),
+                    'black_generation': float(processed_form['black_generation']),
+                    'angle_cyan':float(processed_form['angle_cyan']),
+                    'angle_magenta':float(processed_form['angle_magenta']),
+                    'angle_yellow':float(processed_form['angle_yellow']),
+                    'angle_black':float(processed_form['angle_black']),
                 }
         return render_template('thanks_for_index.html', case_code=case_code, imgheight=imgheight, imgwidth=imgwidth,defaults=defaults)
-    defaults = {
-                    'spacing': 8,
-                    'black_generation': 0.5,
-                    'angle_cyan':165,
-                    'angle_magenta':45,
-                    'angle_yellow':90,
-                    'angle_black':105,
-    }
     return render_template('index.html', defaults=defaults)
 
 @views.route('/index-success/<case_code>')
